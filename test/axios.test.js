343b7449beb4be80603c43b6a47b817f076256f6
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-24 09:16:17 +0700

    http method

--- test/axios.test.js
+++ test/axios.test.js
@@ -1,11 +1,23 @@
 import * as axios from "axios";
 
-describe("HTTP Client", () => {
-  it("should be supported by axios", async () => {
-    const httpClient = axios.create({
-      baseUrl: "https://eni8o9s68oq1q.x.pipedream.net/",
-      timeout: 5000,
-    });
-    expect(httpClient).toBeDefined();
+// describe("HTTP Client", () => {
+//   it("should be supported by axios", async () => {
+//     const httpClient = axios.create({
+//       baseURL: "https://eni8o9s68oq1q.x.pipedream.net/",
+//       timeout: 5000,
+//     });
+//     expect(httpClient).toBeDefined();
+//   });
+// });
+
+describe("HTTP Method", () => {
+  const httpClient = axios.create({
+    baseURL: "https://elhamabdussalam.vercel.app/",
+    timeout: 5000,
+  });
+
+  it("should support GET Method", async () => {
+    const response = await httpClient.get("/");
+    expect(response.status).toBe(200);
   });
 });
