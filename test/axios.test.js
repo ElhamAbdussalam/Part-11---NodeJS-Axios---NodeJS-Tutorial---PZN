cf856894fae198ac1fc4e1692d4bfefc615c1d23
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-24 08:35:26 +0700

    http client

--- test/axios.test.js
+++ test/axios.test.js
@@ -2,7 +2,10 @@ import * as axios from "axios";
 
 describe("HTTP Client", () => {
   it("should be supported by axios", async () => {
-    const httpClient = axios.create();
+    const httpClient = axios.create({
+      baseUrl: "https://eni8o9s68oq1q.x.pipedream.net/",
+      timeout: 5000,
+    });
     expect(httpClient).toBeDefined();
   });
 });
